43d20ee69dcaa0a31525191e00b530613c455f72
monkey <shubhamhpchavda@gmail.com>
2025-10-29 18:35:27 +0530

    Create Billing.java

---
+++ exp4/Billing.java
@@ -0,0 +1,26 @@
+package exp4;
+
+public class Billing {
+    
+    double ComputeBill(double price){
+        double total = price;
+        return (total*0.08) + total;
+    }
+
+    double ComputeBill(double price, int quantity){
+        double total = price * quantity;
+        return (total*0.08) + total;
+    }
+
+    double ComputeBill(double price, int quantity, double coupon){
+        double total = (price*quantity) - coupon;
+        return (total*0.08) + total;
+    }
+
+    public static void main(String[] args){
+        Billing bg = new Billing();
+        System.out.println(bg.ComputeBill(89));
+        System.out.println(bg.ComputeBill(85.22, 6));
+        System.out.println(bg.ComputeBill(96.36, 8, 22.26));
+    }
+}
