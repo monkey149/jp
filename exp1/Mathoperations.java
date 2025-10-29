1675342a970ceb77861a11e86c1306b34f3c9e33
monkey <shubhamhpchavda@gmail.com>
2025-10-29 18:31:24 +0530

    Create Mathoperations.java

---
+++ exp1/Mathoperations.java
@@ -0,0 +1,13 @@
+class Mathoperations{
+    int multiply(int a, int b){
+        return a * b;
+    }
+
+    double multiply(double a, double b){
+        return a * b;
+    }
+
+    int multiply(int a, int b, int c){
+        return a * b * c;
+    }
+}
