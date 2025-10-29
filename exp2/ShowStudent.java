a9a424cb4a4ae95dbd4fe94f9e6217c62ac6b164
monkey <shubhamhpchavda@gmail.com>
2025-10-29 18:32:36 +0530

    Create ShowStudent.java

---
+++ exp2/ShowStudent.java
@@ -0,0 +1,10 @@
+package exp2;
+
+public class ShowStudent {
+    public static void main(String[] args){
+        Student st = new Student();
+        st.studentInfo(3, 6, 16);
+        st.gradePointAverage();
+        st.display();
+    }
+}
